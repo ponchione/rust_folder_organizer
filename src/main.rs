537c5f4042c56a0a16e4c7c32e2ee3c1bfd0e18c
Mitchell Ponchione <mitchell.ponchione@protonmail.com>
2020-08-31 15:30:05 -0400

    Added functionality for looping over a folder and collecting the file extensions.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
-use std::path::Path;
-use std::env;
+#[allow(dead_code)]
+
+// use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::fs::DirEntry;
+use std::ffi::OsStr;
+use std::io;
 
 fn main() {
 
-    let current = env::current_dir();
+    let work_dir: &str = "E:\\TestFolder\\";
+    let dir: &Path = Path::new(work_dir);
+
+    if dir.is_dir() {
+        let extensions: Vec<String> = collect_extensions(&dir);
+
+        for extension in extensions.iter() {
+            println!("final extension: {}", extension);
+        }
+    }
+}
 
 
+fn collect_extensions(dir: &Path) -> Vec<String> {
+    let mut exts: Vec<String> = Vec::new();
 
+    for item in fs::read_dir(dir).unwrap() {
+        let entry = item.unwrap();
+        let path = entry.path();
+        let ext = path.extension();
 
+        if let Some(os_str) = ext {
+            if let Some(ext_str) = os_str.to_str() {
+                exts.push(String::from(ext_str));
+            }
+        }
+    }
 
-
+    exts
 }
